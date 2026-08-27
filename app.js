81a012ab9e13b8532a3c2910affe97c1d69190ff
Ruelas <sruelas@una.edu>
2026-08-27 13:32:26 -0500

    sinetgubg

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.use(express.static(join(__dirname, 'public')));
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
